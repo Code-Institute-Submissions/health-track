f37666ff133c8766f154164291ef155a9bdeebb5
farhansam <farhan_samsol@hotmail.com>
2021-01-18 09:46:26 +0800

    added counter group

--- js/script.js
+++ js/script.js
@@ -1,5 +1,17 @@
 $(document).ready(function () {
+
+    //$('.calculator').hide();
+    //$('.counter').hide();
     
+    $('.cardCalculator').click(function(){
+        $('.mainDisplay').hide();
+        $('.calculator').show();
+    })
+
+    $('.cardCounter').click(function(){
+        $('.mainDisplay').hide();
+        $('.counter').show();
+    })
     
 
 
